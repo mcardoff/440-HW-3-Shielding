df738c0206941f9e6b2c1e636483384a938fdf74
Michael Cardiff <mcardiff@hawk.iit.edu>
2022-02-14 16:47:31 -0600

    Initial Commit

---
+++ Shared/_40_HW_3_ShieldingApp.swift
@@ -0,0 +1,17 @@
+//
+//  _40_HW_3_ShieldingApp.swift
+//  Shared
+//
+//  Created by Michael Cardiff on 2/14/22.
+//
+
+import SwiftUI
+
+@main
+struct _40_HW_3_ShieldingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
